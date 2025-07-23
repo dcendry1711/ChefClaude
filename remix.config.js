881b8729c23a881457d01a59d98b4ecb507c998d
dcendry1711 <daniel.cendry@gmail.com>
2025-07-23 19:29:57 +0200

    pop

---
+++ remix.config.js
@@ -0,0 +1,8 @@
+/** @type {import('@remix-run/dev').AppConfig} */
+module.exports = {
+  ignoredRouteFiles: ["**/.*"],
+  future: {
+    v3_routeConvention: true
+  },
+  serverModuleFormat: "esm"
+};
